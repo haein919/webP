80733997f94ff5d13f2674e2c62cefb1c1138536
haein919 <haein919@gmail.com>
2020-11-24 18:04:56 +0900

    elle메인

--- c_code/elle/js/src/elle.js
+++ c_code/elle/js/src/elle.js
@@ -1,14 +1,18 @@
 var nowT = deviceType();
 // console.log('device체크: ' , nowT);
-
+var body = $('body');
 
 (function($){
   var dType = ['mob','tab','lap','pc'];
   var wrap = $('#wrap');
   if(nowT === dType[0]){
-    wrap.load('./elleMain/elle_m_mob.html');
+    wrap.load('./elleMain/elle_m_mob.html', function(){
+      body.append('<script src="../js/src/elle_m_mob.js"></script>');
+    });
   }else{
-    wrap.load('./elleMain/elle_m_other.html');
+    wrap.load('./elleMain/elle_m_other.html', function(){
+      body.append('<script src="../js/src/elle_m_other.js"></script>');
+    });
   }
 
 })(jQuery);

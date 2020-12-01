3d0757494bbe7789fea470534860e29906ba8af6
unknown <haein919@gmail.com>
2020-12-01 23:09:47 +0900

    elle

--- c_code/elle/js/src/elle_search.js
+++ c_code/elle/js/src/elle_search.js
@@ -13,15 +13,9 @@
   // issueUl.append(copyLi);
   // var reIssueLi = issueUl.find('li');
   // var textN = 0;
-  
-  issueLi.slideUp({
-    animationtype: 'slide',    //animationtype: 'fade', animationtype: 'slide',
-    speed: 2000,      //speed:'slow' , speed: 'normal' , speed: 'fast'   milliseconds
-    timeout: 2000,      //milliseconds
-    type: 'sequence',     //type: 'random', type: 'random_start',
-    containerheight: '1em'    //containerheight:'auto' , containerheight: '1em'   css-height-value
-    //runningclass: 'innerfade'   //CSS-Class which the container get's applied ( Default : 'innerfade')
- });
+  issueLi.slideUp(function(){
+    $(this).slideDown();
+  });
 
 
  // jQuery end
